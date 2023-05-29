00feeaef3a11a2756d81a31f391399c5a61b75cc
Sebastian.ramirez <124749988+SebastianEci@users.noreply.github.com>
2023-05-28 23:09:05 -0500

    Update db_local.cpp

--- db_local.cpp
+++ db_local.cpp
@@ -6,7 +6,15 @@ include "db_local.h"
 using namespace std;
 
 DB_Local::DB_Local( string url ){
-
+    database.exec("CREATE TABLE IF NOT EXISTS mediciones ("
+                  "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                  "fecha_hora TEXT,"
+                  "temperatura REAL,"
+                  "humedad REAL,"
+                  "precipitacion REAL,"
+                  "intensidad_luz REAL"
+                  ")");
+  
 }
 
 bool DB_Local::Abrir_DB(){
